0e5056e567290429c4ae87922371c4182a63d316
Alex Milde <alex@finekost.com>
2023-01-12 16:39:59 +0100

    #retest

---
+++ app/models/test.server.ts
@@ -0,0 +1,7 @@
+import { PrismaClient, test } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export function getReviews() {
+    return prisma.test.findMany();
+}
--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,10 +1,8 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import Rating from "~/components.tsx/rating";
-import { XCircleIcon } from "@heroicons/react/20/solid";
-
-import { getReviews } from "~/models/review.server";
+import { getReviews } from "~/models/test.server";
 
 export async function loader({ request }: LoaderArgs) {
   const noteListItems = await getReviews();
@@ -22,15 +20,11 @@ export default function Reviews() {
 
 
         {data.noteListItems.length === 0 ? (
-          <p className="p-4">No notes yet</p>
+          <p className="p-4">No tests yet</p>
         ) : (
           <div>
             {data.noteListItems.map((review) => (
               <div key={review.id}>
-                <h2 className="mb-4 text-sm font-medium text-gray-900">
-                  {review.name}
-                </h2>
-                <Rating rating={review.rating}></Rating>
                 <p className="prose prose-sm mt-4 text-gray-500">
                   {review.text}
                 </p>
