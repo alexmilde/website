8de0fb125ca40ddcad3386f00d88be6fa5ae8f4d
Alex Milde <alex@finekost.com>
2023-01-12 21:52:06 +0100

    changed title. moved build step to server

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Alex Milde / Developer",
   viewport: "width=device-width,initial-scale=1",
 });
 
