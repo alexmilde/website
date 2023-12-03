07f1883183c146f428fd30d6750d95ff6c27d291
Alex Milde <alex@finekost.com>
2023-12-03 21:00:42 +0100

    #main added font some texts and basic routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,41 @@
-<h1>Hello World.</h1>
+<script lang="ts">
+    const headline = 'font-bauhaus text-3xl'
+    const container = 'mx-5'
+    const wrapper = 'mt-20'
+
+    const texts = {
+        about: {
+            hl: 'über mich',
+            cpy: 'Moin, ich bin Alex. Software engineer, Architect – Fullstack developer. I do nerd things.',
+            cta: 'kontakt',
+        },
+        debateable: {
+            hl: 'debatebale',
+            cpy: 'Nach sehr vielen Jahren und etlichen Projekten sammelt man Einsichten und Ansichten. Zu Software und Leadership. Subjektiv und debatebale.',
+            cta: 'kontakt',
+        },
+        contact: {
+            hl: 'kontakt',
+            cpy: 'Lass uns gern sprechen. Vielleicht kann ich ja weiter helfen. ',
+        },
+    }
+</script>
+
+<div class={container}>
+    <div class={wrapper}>
+        <h1 class={headline}>{texts.about.hl}</h1>
+        <p>{texts.about.cpy}</p>
+        <button>{texts.about.cta}</button>
+    </div>
+
+    <div class={wrapper}>
+        <h1 class={headline}><a href="/debateable">{texts.debateable.hl}</a></h1>
+        <p>{texts.debateable.cpy}</p>
+        <button>{texts.debateable.cta}</button>
+    </div>
+
+    <div class={wrapper}>
+        <h1 class={headline}>{texts.contact.hl}</h1>
+        <p>{texts.contact.cpy}</p>
+    </div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,33 @@
 /** @type {import('tailwindcss').Config}*/
+
+const plugin = require('tailwindcss/plugin')
+
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+    content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {}
-  },
+    theme: {
+        extend: {
+            fontFamily: {
+                bauhaus: [
+                    'Bauhaus',
+                    'ui-sans-serif, system-ui, -apple-system, "system-ui", "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+                ],
+            },
+        },
+    },
 
-  plugins: []
-};
+    plugins: [
+        plugin(function ({ addBase }) {
+            addBase({
+                '@font-face': {
+                    fontFamily: 'Bauhaus',
+                    fontStyle: 'normal',
+                    fontWeight: '400',
+                    src: 'url(/fonts/Bauhaus_.ttf)',
+                },
+            })
+        }),
+    ],
+}
 
-module.exports = config;
+module.exports = config
